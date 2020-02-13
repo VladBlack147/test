da7ce9dc1f9e4b0077dcfb6df7d3878b290f8233
Black <obmen3018@gmail.com>
2020-02-13 05:59:17 +0200

    Это наш третий комит

--- Project_v2/script.js
+++ Project_v2/script.js
@@ -6,10 +6,10 @@ let money,
   answer,
   everyDay;
 
-money = prompt(`Ваш бюджет на месяц?`);
-time = prompt(`Введите дату в формате YYYY-MM-DD`);
-expens = prompt(`Введите обязательную статью расходов в этом месяце`);
-answer = prompt(`“Во сколько обойдется?”`);
+money = prompt(`Ваш бюджет на месяц?`, `40000`);
+time = prompt(`Введите дату в формате YYYY-MM-DD`, `2020 30 01`);
+expens = prompt(`Введите обязательную статью расходов в этом месяце`, `T`);
+answer = prompt(`Во сколько обойдется?`, `1000`);
 
 
 let appData = {
