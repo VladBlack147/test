dea19ed1668c15978e11e60906672936665f1fb1
Black <obmen3018@gmail.com>
2020-02-13 05:14:57 +0200

    Это наш третий комит

--- Project_v2/script.js
+++ Project_v2/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+let money = prompt(`Ваш бюджет на месяц?`);
+let time = prompt(`Введите дату в формате YYYY-MM-DD`);
+
+let answer1 = prompt (`Введите обязательную статью расходов в этом месяце`);
+let answer2 = prompt (`Во сколько обойдется?`);
+
+
+let appData = {
+  budjet: money,
+  timeData: time,
+  expenses: {
+    answer1 : answer2
+   },
+  optionalExpenses: "",
+  income: "",
+  savings: false
+};
+
+let dayExp = money / 30; 
+
+alert(dayExp);
+
