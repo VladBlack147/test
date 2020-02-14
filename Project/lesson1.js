d7a33db8dec7c62cb6b0f2697b8f84bd46eeb6d4
Black <obmen3018@gmail.com>
2020-02-14 05:52:51 +0200

    Это наш третий комит

--- Project/lesson1.js
+++ Project/lesson1.js
@@ -29,6 +29,42 @@ for (let i = 0; i < 2; i++) {
 }
 
 
+/* let i = 0;
+
+do {
+
+  let a = prompt(`Введите обязательную статью расходов в этом месце`, '');
+  let b = prompt(`Во сколько обойдется?`, '');
+  appData.expenses[a] = b;
+  i++;
+  if ((typeof (a)) === 'string' && (typeof (a)) != null &&
+  (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+  console.log("done");
+  appData.expenses[a] = b;
+  
+} else {
+    i = i - 1;
+  }
+
+}
+while (i < 2); */
+
+/* let i = 0;
+while (i < 2) {
+  let a = prompt(`Введите обязательную статью расходов в этом месце`, '');
+  let b = prompt(`Во сколько обойдется?`, '');
+  appData.expenses[a] = b;
+  i++;
+  if ((typeof (a)) === 'string' && (typeof (a)) != null &&
+    (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+    console.log("done");
+    appData.expenses[a] = b;
+
+  } else {
+    i = i - 1;
+  }
+} */
+
 
 appData.moneyPerDay = appData.budjet / 30;
 
